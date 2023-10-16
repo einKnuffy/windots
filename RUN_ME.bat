243e1bebafbd0bf0eb6d1237ac510aeb4cb79f3f
einKnuffy <66639485+einKnuffy@users.noreply.github.com>
2023-10-16 18:00:04 +0200

    Update RUN_ME.bat

--- RUN_ME.bat
+++ RUN_ME.bat
@@ -1,5 +1,5 @@
-rmdir %localappdata%\windots\
-xcopy /s . %localappdata%\windots\
+rmdir /s /q %localappdata%\windots\
+xcopy /s /q . %localappdata%\windots\
 
 ECHO "This script will now self-destruct. Please ignore the next error message"
 ECHO "YOU CAN NOW DELETE THE FOLDER. WAIT A BIT"
